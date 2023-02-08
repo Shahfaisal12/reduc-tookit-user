914bee0b4a81b1051548c59cb21ea7b1a9992ef9
shahfaisal12 <faisalmkd02@gmail.com>
2023-02-08 12:39:15 +0500

    'add-counterreducer'

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -1,14 +1,28 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import DeleteUser from './DeleteUser'
 import { fakeUserData } from '../utils/api'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addUser } from '../../pages/store/slice/UserSlice'
 import DisplayUsers from './DisplayUsers'
+import { increment, decrement, reset, incrementByAmount } from '../../pages/store/slice/Counterslice'
 
 const Hero = () => {
 
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
   const dispatch = useDispatch();
+
+  const addvalue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  }
+
+  const count = useSelector((state) => 
+    state.counter.count
+  )
 
   const addNewUser = (name) => {
     console.log("hero add user", name);
@@ -44,6 +58,25 @@ const Hero = () => {
           </div>
           <hr />
           <DeleteUser />
+        </div>
+
+        <div className="row mt-5">
+          <div className="counter-section text-center">
+            <h1>Counter section</h1>
+            <div class="btn-group btn-group-lg mt-4" role="group" aria-label="...">
+              <button type="button" className='btn btn-outline-danger' onClick={() => dispatch(decrement())}>-</button>
+              <button type="button" className='btn btn-outline-primary'>{count}</button>
+              <button type="button" className='btn btn-outline-success' onClick={() => dispatch(increment())}>+</button>
+            </div>
+
+            <div className="rest-section mt-4 ">
+            <input type="text" value={incrementAmount} className='' onChange={(e)=>setIncrementAmount(e.target.value)} />
+            <div className="btn-dev mt-4">
+            <button type="button" className='btn btn-outline-success mx-3'  onClick={() => dispatch(incrementByAmount(addvalue))}>AddAmount</button>
+            <button type="button" className='btn btn-outline-success' onClick={resetAll}>Reset</button>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/store/index.jsx
+++ src/pages/store/index.jsx
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slice/UserSlice";
+import counterSlice from './slice/Counterslice';
+// import counterreducer from './slice/Counterslice'; you may also write like this
 
 const store = configureStore({
   reducer: {
     users: userSlice,
+    counter: counterSlice,
   },
 });
 

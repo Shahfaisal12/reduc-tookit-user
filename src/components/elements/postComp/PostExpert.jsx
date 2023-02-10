3524710d84f8c8985a69ff24407bfac2970cce70
shahfaisal12 <faisalmkd02@gmail.com>
2023-02-10 15:29:59 +0500

    'add-style-card'

--- src/components/elements/postComp/PostExpert.jsx
+++ src/components/elements/postComp/PostExpert.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 import PostAuthor from "./PostAuthor";
 import ReactionButton from "./ReactionButton";
 import TimeAgo from "./TimeAgo";
 
-
 const PostExpert = ({ post }) => {
   return (
     <div className="col-md-6">
-      <section>
-        <h3>{post.title}</h3>
-        <p>{post.body.substring(0, 100)}</p>
-        <p className="postCredit">
-          <PostAuthor userId={post.userId} />
-          <TimeAgo timestamp={post.date} />
-          <ReactionButton post={post} />
-        </p>
+      <section className="d-flex flex-column justify-content-between" style={{ minHeight: "300px" }}>
+        <h4>{post.title}</h4>
+        <div>
+          <p>{post.body.substring(0, 100)}</p>
+          <p className="postCredit" style={{ fontSize: "11px" }}>
+            <PostAuthor userId={post.userId} />
+            <TimeAgo timestamp={post.date} />
+          </p>
+        </div>
+        <ReactionButton post={post} />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default PostExpert
+export default PostExpert;
--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -1,52 +1,93 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { ImCart } from 'react-icons/im'
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ImCart } from "react-icons/im";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-
   const items = useSelector((state) => state.cart);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand text-white" href="/">RelymerLabs</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active text-white" aria-current="page" to="/">TodoList</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/counter">Counter</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/post">Post</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/shop">Shop</Link>
-        </li>
-      </ul>
-          <div className="my-cart position-relative">
-            <ImCart className='fs-2 text-danger' />
-            
-              <span className='bg-danger rounded-circle m-auto fs-6 position-absolute text-white' style={{ top: '-8px', left: '10px', zIndex: '22', padding: '0px 5px' }}>{items.length}</span>
-           
-          </div>
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="/">
+          RelymerLabs
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className="nav-link active text-white"
+                aria-current="page"
+                to="/"
+              >
+                TodoList
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/counter">
+                Counter
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/post">
+                Post
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/shop">
+                Shop
+              </Link>
+            </li>
+          </ul>
+
           <div className="mx-0 mx-md-4 my-3 py-md-0">
-            <Link className="text-white text-decoration-none fs-6 fw-bold" to="/cart">Cart</Link>
+            <Link
+              className="my-cart position-relative text-white text-decoration-none fs-6 fw-bold "
+              to="/cart"
+            >
+                <ImCart className="fs-2 text-danger" />
+                <span
+                  className="bg-danger rounded-circle m-auto fs-6 position-absolute text-white"
+                  style={{
+                    top: "-8px",
+                    left: "10px",
+                    zIndex: "22",
+                    padding: "0px 5px",
+                  }}
+                >
+                  {items.length}
+                </span>
+            </Link>
           </div>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success text-white" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success text-white"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
